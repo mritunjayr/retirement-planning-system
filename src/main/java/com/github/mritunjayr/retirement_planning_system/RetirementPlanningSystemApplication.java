662e94ee54f858043d971da3246cc75c2051c12a
Mritunjay Kumar <mritunjaykumar694@gmail.com>
2026-02-21 11:13:42 +0530

    project initialized

---
+++ src/main/java/com/github/mritunjayr/retirement_planning_system/RetirementPlanningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.mritunjayr.retirement_planning_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementPlanningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementPlanningSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mritunjayr/retirement_planning_system/RetirementPlanningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mritunjayr.retirement_planning_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetirementPlanningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
